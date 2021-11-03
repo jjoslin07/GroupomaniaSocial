f82797e44a67712370fe27dfcf90d43176b3e8c1
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2021-11-02 19:45:52 -0700

    Add follow user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -17,27 +17,27 @@ router.put("/:id", async (req, res) => {
             const user = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
             });
-            res.status(200).json("Account has been updated")
+            res.status(200).json("Account has been updated");
         } catch (error) {
             return res.status(500).json(error);
         }
     } else {
         return res.status(403).json("You can only update your account");
     }
-})
+});
 // Delete user
 router.delete("/:id", async (req, res) => {
     if (req.body.userId === req.params.id || req.body.isAdmin) {
         try {
             const user = await User.findByIdAndDelete(req.params.id);
-            res.status(200).json("Account has been deleted")
+            res.status(200).json("Account has been deleted");
         } catch (error) {
             return res.status(500).json(error);
         }
     } else {
         return res.status(403).json("You can only delete your account");
     }
-})
+});
 // Get a user
 router.get("/:id", async (req, res) => {
     try {
@@ -46,15 +46,42 @@ router.get("/:id", async (req, res) => {
             password,
             updatedAt,
             ...other
-        } = user._doc
+        } = user._doc;
         res.status(200).json(other);
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
 // Follow a user
+
+router.put("/:id/follow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({
+                    $push: {
+                        followers: req.body.userId
+                    }
+                });
+                await currentUser.updateOne({
+                    $push: {
+                        following: req.params.id
+                    }
+                });
+                res.status(200).json("User has been followed");
+            } else {
+                res.status(403).json("You already follow this user");
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json("You can't follow yourself");
+    }
+});
 
 // Unfollow a user
 
-
-module.exports = router
+module.exports = router;
