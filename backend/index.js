c14326a74545437330709a61a1b19faa6de0eaf2
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2021-11-02 20:24:02 -0700

    Create post model
    Create post model
    
    Init post route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
+const {
+    post
+} = require("./routes/post");
 
 
 dotenv.config();
@@ -25,6 +29,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 
 app.listen(3001, () => {
---
+++ backend/routes/post.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+// Create a post
+
+// Update a post
+
+// Delete a post
+
+// Get a post
+
+// Get timeline posts
+
+
+module.exports = router;
