9f1a29e74949399fb627490145f99cf95c8eea91
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2021-11-02 19:50:52 -0700

    Unfollow user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -84,4 +84,32 @@ router.put("/:id/follow", async (req, res) => {
 
 // Unfollow a user
 
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({
+                    $pull: {
+                        followers: req.body.userId
+                    }
+                });
+                await currentUser.updateOne({
+                    $pull: {
+                        following: req.params.id
+                    }
+                });
+                res.status(200).json("User has been unfollowed");
+            } else {
+                res.status(403).json("You are not following this user");
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(403).json("You can't unfollow yourself");
+    }
+});
+
 module.exports = router;
