da0512800df2c10ebd7a303df159255daf6c9c81
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2021-11-09 16:07:01 -0800

    Update user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,77 +1,71 @@
-const mongoose = require("mongoose");
-
-// Use mongoose plugin to require unique email in the signup process.
-const uniqueValidator = require('mongoose-unique-validator');
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("sqlite::memory:");
 
 // Fucntion to require email validation before signup
 let validateEmail = (email) => {
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return re.test(email);
 };
+module.exports = (sequelize, DataTypes) => {
+class User extends Model {}
 
-const userSchema = new mongoose.Schema({
-        username: {
-            type: String,
-            require: true,
-            min: 3,
-            max: 20,
-            unique: true
-        },
-        email: {
-            type: String,
-            required: true,
-            lowercase: true,
-            unique: true,
-            max: 50,
-            validate: [validateEmail, 'Please fill a valid email address']
-        },
-        password: {
-            type: String,
-            required: true,
-            min: 6
-        },
-        profilePicture: {
-            type: String,
-            default: "",
-        },
-        coverPicture: {
-            type: String,
-            default: ""
-        },
-        followers: {
-            type: Array,
-            default: []
-        },
-        following: {
-            type: Array,
-            default: []
-        },
-        isAdmin: {
-            type: Boolean,
-            default: false,
-        },
-        desc: {
-            type: String,
-            max: 50
-        },
-        city: {
-            type: String,
-            max: 50
-        },
-        from: {
-            type: String,
-            max: 50
-        },
-        relationship: {
-            type: Number,
-            enum: [1, 2, 3]
-        },
-    }, {
-        timestamps: true
-    }
-
-);
-
-userSchema.plugin(uniqueValidator);
-
-module.exports = mongoose.model('User', userSchema);
+User.init({
+  // Model attributes are defined here
+  username: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+    unique: true
+},
+    email: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    lowercase: true,
+    unique: true,
+    validate: [validateEmail, 'Please fill a valid email address']
+},
+password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+},
+profilePicture: {
+    type: DataTypes.BLOB,
+    default: null
+},
+coverPicture: {
+    type: DataTypes.BLOB,
+    default: null
+},
+followers: {
+    type: DataTypes.JSON,
+    default: null
+},
+following: {
+    type: DataTypes.JSON,
+    default: null
+},
+isAdmin: {
+    type: DataTypes.BOOLEAN,
+    default: false,
+},
+desc: {
+    type: DataTypes.STRING(500),
+},
+city: {
+    type: DataTypes.STRING(50),
+},
+from: {
+    type: DataTypes.STRING(50),
+},
+relationship: {
+    type: DataTypes.ENUM,
+    enum: [1, 2, 3]
+},
+}, {
+timestamps: true
+},
+ {
+  sequelize,
+  modelName: 'User' 
+});
+return User
+}
