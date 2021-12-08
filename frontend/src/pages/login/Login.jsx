7dc6e030667f911ab519ea3ecb2fc02e6f4ac0c6
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2021-12-07 17:21:23 -0800

    :Wrap App with AuthContext to manage state

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,15 @@
+import { useRef } from "react";
 import "./login.css";
 
 const Login = () => {
+	const email = useRef();
+	const password = useRef();
+
+	const handleClick = (e) => {
+		e.preventDefault();
+		console.log(email.current.value);
+	};
+
 	return (
 		<div className="login">
 			<div className="loginWrapper">
@@ -11,14 +20,27 @@ const Login = () => {
 					</span>
 				</div>
 				<div className="loginRight">
-					<div className="loginBox">
-						<input placeholder="Email" className="loginInput" />
-						<input placeholder="Password" className="loginInput" />
+					<form className="loginBox" onSubmit={handleClick}>
+						<input
+							placeholder="Email"
+							type="email"
+							required
+							className="loginInput"
+							ref={email}
+						/>
+						<input
+							placeholder="Password"
+							type="password"
+							required
+							minLength="6"
+							className="loginInput"
+							ref={password}
+						/>
 						<button className="loginButton">Log In</button>
 						<span className="loginForgot">Forgot Password?</span>
 
 						<button className="loginRegisterButton">Register Account</button>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
