5567ffe9e814d7dbf616c7d7ee448e08a4f86bef
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2021-12-14 01:53:02 -0800

    :Update account page

--- frontend/src/pages/account/Account.jsx
+++ frontend/src/pages/account/Account.jsx
@@ -1,5 +1,6 @@
 // import { Visibility, VisibilityOff } from "@mui/icons-material";
 import {
+	Button,
 	// IconButton,
 	// Input,
 	// InputAdornment,
@@ -9,52 +10,141 @@ import {
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import axios from "axios";
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import Topbar from "../../components/topbar/Topbar";
 import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import LoadingButton from "@mui/lab/LoadingButton";
+import SaveIcon from "@mui/icons-material/Save";
+
 import "./account.css";
 
 const Account = () => {
+	// Navigate const for page redirection
+	const navigate = useNavigate();
+	// Set user to Alias to currentUser
 	const { user: currentUser } = useContext(AuthContext);
-	// const config = {
-	// 	headers: { Authorization: `Bearer ${currentUser.token}` },
+	// Send user token to header
+	const config = {
+		headers: { Authorization: `Bearer ${currentUser.token}` },
+	};
+	// useState
+	const [username, setUsername] = useState(currentUser.user.username);
+	// const username = useRef();
+	const [email, setEmail] = useState(currentUser.user.email);
+	// const email = useRef();
+	const [password, setPassword] = useState(currentUser.user.password);
+	// const pass = useRef();
+	const [passwordVerify, setPasswordVerify] = useState(
+		currentUser.user.password
+	);
+	// const verify = useRef();
+	const [profilePicture, setProfilePicture] = useState(
+		currentUser.user.profilePicture
+	);
+	// const profilePicture = useRef();
+	const [coverPicture, setCoverPicture] = useState(
+		currentUser.user.coverPicture
+	);
+	// const coverPicture = useRef();
+	// const desc = useRef();
+	const [desc, setDesc] = useState(currentUser.user.desc);
+	const [city, setCity] = useState(currentUser.user.city);
+	// const city = useRef();
+	const [from, setFrom] = useState(currentUser.user.from);
+	// const from = useRef();
+	const [dept, setDept] = useState(currentUser.user.dept);
+	// const dept = useRef();
+	const [year, setYear] = useState(currentUser.user.year);
+	// const year = useRef();
+	// console.log(currentUser.token);
+	// const submitHandler = async (e) => {
+	// 	e.preventDefault();
+	// 	await axios.put(
+	// 		"/users/" + currentUser.user.id,
+	// 		{
+	// 			username: username.current.value,
+	// 			email: email.current.value,
+	// 			password: password.current.value,
+	// 			passwordVerify: passwordVerify.current.value,
+	// 			profilePicture: profilePicture.current.value,
+	// 			coverPicture: coverPicture.current.value,
+	// 			desc: desc.current.value,
+	// 			city: city.current.value,
+	// 			from: from.current.value,
+	// 			dept: dept.current.value,
+	// 			year: year.current.value,
+	// 		},
+	// 		{
+	// 			headers: {
+	// 				Authorization: `Bearer ${currentUser.token}`,
+	// 			},
+	// 		}
+	// 	);
 	// };
-	const username = useRef();
-	const email = useRef();
-	const password = useRef();
-	const passwordVerify = useRef();
-	const profilePicture = useRef();
-	const coverPicture = useRef();
-	const desc = useRef();
-	const city = useRef();
-	const from = useRef();
-	const dept = useRef();
-	const year = useRef();
-	console.log(currentUser.token);
+
+	// Handle change for form
+	const handleChangeName = (event) => {
+		setUsername(event.currentTarget.value);
+	};
+	const handleChangeEmail = (event) => {
+		setEmail(event.currentTarget.value);
+	};
+	const handleChangePass = (event) => {
+		setPassword(event.currentTarget.value);
+	};
+	const handleChangePassVerify = (event) => {
+		setPasswordVerify(event.currentTarget.value);
+	};
+	const handleChangeDesc = (event) => {
+		setDesc(event.currentTarget.value);
+	};
+	const handleChangeCity = (event) => {
+		setCity(event.currentTarget.value);
+	};
+	const handleChangeFrom = (event) => {
+		setFrom(event.currentTarget.value);
+	};
+	const handleChangeDept = (event) => {
+		setDept(event.currentTarget.value);
+	};
+	const handleChangeYear = (event) => {
+		setYear(event.currentTarget.value);
+	};
+
 	const submitHandler = async (e) => {
 		e.preventDefault();
-		axios.put(
-			"/users/" + currentUser.user.id,
-			{
-				username: username.current.value,
-				email: email.current.value,
-				password: password.current.value,
-				passwordVerify: passwordVerify.current.value,
-				profilePicture: profilePicture.current.value,
-				coverPicture: coverPicture.current.value,
-				desc: desc.current.value,
-				city: city.current.value,
-				from: from.current.value,
-				dept: dept.current.value,
-				year: year.current.value,
-			},
-			{
-				headers: {
-					Authorization: `Bearer ${currentUser.token}`,
-				},
-			}
-		);
+		// if (passwordVerify.value !== password.current.value) {
+		// 	passwordVerify.current.setCustomValidity("Passwords don't match!");
+		// } else {
+		const user = {
+			username: username,
+			email: email,
+			password: password,
+			passwordVerify: passwordVerify,
+			profilePicture: profilePicture,
+			coverPicture: coverPicture,
+			desc: desc,
+			city: city,
+			from: from,
+			dept: dept,
+			year: year,
+		};
+		try {
+			await axios.put("/users/" + currentUser.user.id, user, config);
+		} catch (error) {
+			console.log(error);
+		}
+		// }
+		// await window.localStorage.clear();
+		// await window.sessionStorage.clear();
+		// await window.location.reload(false);
+		await navigate("/profile/" + currentUser.user.username);
 	};
+	const [loading, setLoading] = React.useState(false);
+	function handleClick() {
+		setLoading(true);
+	}
 
 	return (
 		<>
@@ -81,23 +171,23 @@ const Account = () => {
 						>
 							<TextField
 								id="outlined-start-adornment"
-								placeholder="Name"
+								// placeholder="Name"
 								required
-								ref={username}
 								className="accountInput"
 								type="text"
-								defaultValue={currentUser.user.username}
-								label="required"
+								label="Name"
+								onChange={handleChangeName}
+								// defaultValue={currentUser.user.username}
 							/>
 							<TextField
 								id="outlined-start-adornment"
-								placeholder="Email"
+								// placeholder="Email"
 								required
-								ref={email}
 								className="accountInput"
 								type="email"
-								defaultValue={currentUser.user.email}
-								label="required"
+								label="Email"
+								onChange={handleChangeEmail}
+								// defaultValue={currentUser.user.email}
 							/>
 
 							{/* <OutlinedInput
@@ -125,76 +215,88 @@ const Account = () => {
 								label="Password"
 							/> */}
 							<TextField
-								placeholder="Password"
+								// placeholder={currentUser.user.password}
 								required
-								ref={password}
 								className="accountInput"
 								type="password"
 								minLength="6"
 								label="password"
+								// ref={pass}
+								onChange={handleChangePass}
 							/>
 							<TextField
-								placeholder="Verify Password"
+								// placeholder="Verify Password"
 								required
-								ref={passwordVerify}
 								className="accountInput"
 								type="password"
 								minLength="6"
 								label="verify password"
+								// ref={verify}
+								onChange={handleChangePassVerify}
 							/>
+
 							<TextField
 								placeholder="profilePicture"
-								ref={profilePicture}
 								className="accountInput"
-								type="email"
+								type="string"
 							/>
 							<TextField
 								placeholder="coverPicture"
-								ref={coverPicture}
 								className="accountInput"
-								type="email"
+								type="string"
 							/>
 							<TextField
-								placeholder="Description"
-								ref={desc}
+								// placeholder="Description"
 								className="accountInput"
 								type="text"
-								defaultValue={currentUser.user.desc}
+								label="Description"
+								// defaultValue={currentUser.user.desc}
+								onChange={handleChangeDesc}
 							/>
 							<TextField
-								placeholder="City"
-								ref={city}
+								label="City"
 								className="accountInput"
 								type="text"
-								defaultValue={currentUser.user.city}
+								// defaultValue={currentUser.user.city}
+								// onChange={submitHandler}
+								// value={city}
+								onChange={handleChangeCity}
 							/>
 							<TextField
-								placeholder="Hometown"
-								ref={from}
+								label="Hometown"
 								className="accountInput"
 								type="text"
-								defaultValue={currentUser.user.from}
+								onChange={handleChangeFrom}
+								// defaultValue={currentUser.user.from}
 							/>
 							<TextField
-								placeholder="Department"
-								ref={dept}
+								label="Department"
 								className="accountInput"
 								type="text"
-								defaultValue={currentUser.user.dept}
+								onChange={handleChangeDept}
+								// defaultValue={currentUser.user.dept}
 							/>
 							<TextField
-								placeholder="Years at Company"
-								ref={year}
+								label="Years at Company"
 								className="accountInput"
 								type="number"
+								onChange={handleChangeYear}
 							/>
 							<div className="accountButtonContainer">
-								<button className="accountCancelButton" type="submit">
+								{/* <button className="accountCancelButton" type="submit">
 									Cancel
-								</button>
-								<button className="accountSaveButton" type="submit">
+								</button> */}
+								{/* <button className="accountSaveButton" type="submit">
 									Save
-								</button>
+								</button> */}
+
+								<Button
+									type="submit"
+									variant="contained"
+									endIcon={<SaveIcon />}
+								>
+									Save
+								</Button>
 							</div>
 						</Box>
 					</div>
