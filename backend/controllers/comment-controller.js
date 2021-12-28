8a5d5f0732a8cc90889d1ebab700512b089ca025
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2021-12-27 16:08:23 -0800

    :read unread switch added

--- backend/controllers/comment-controller.js
+++ backend/controllers/comment-controller.js
@@ -1,39 +1,45 @@
+// Import fastest validator
 const Validator = require("fastest-validator");
+// Import models
 const models = require("../models");
 /**
- *
- * @param {*} req
- * @param {*} res
+ * Saves a comment made by current user to database.
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
  * @returns
  */
 function save(req, res) {
+	// Comment Object
 	const comment = {
 		content: req.body.content,
 		postId: req.params.id,
 		userId: req.userData.userId,
 	};
-
+	//	Schema object for validation
 	const schema = {
 		content: { type: "string", optional: false },
 	};
-
+	// const v for new Validator object
 	const v = new Validator();
+	// validate comment against schema
 	const validationResponse = v.validate(comment, schema);
-
+	// If validation response not equal to true return message and error
 	if (validationResponse !== true) {
 		return res.status(400).json({
 			message: "Validation failed",
 			errors: validationResponse,
 		});
 	}
-
+	// If validation true find post by id
 	models.Post.findByPk(req.params.id)
 		.then((post) => {
+			// If no post found return message
 			if (post === null) {
 				res.status(404).json({
 					message: "Post not found",
 				});
 			} else {
+				// If post found create new comment
 				models.Comment.create(comment)
 					.then((result) => {
 						res.status(201).json({
@@ -57,20 +63,23 @@ function save(req, res) {
 		});
 }
 /**
- *
- * @param {*} req
- * @param {*} res
+ * Shows one comment
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
  */
 function show(req, res) {
+	// id in parameter
 	const id = req.params.id;
-
+	// Look for comment by id
 	models.Comment.findByPk(id)
 		.then((result) => {
+			// If found return comment in response
 			if (result) {
 				res.status(200).json({
 					comment: result,
 				});
 			} else {
+				// If not found return 404 error and message
 				res.status(404).json({
 					message: "Comment not found!",
 				});
@@ -83,20 +92,23 @@ function show(req, res) {
 		});
 }
 /**
- *
- * @param {*} req
- * @param {*} res
+ * Shows ALL comments
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
  */
 function showAll(req, res) {
+	// id of post
 	const postId = req.params.id;
-
+	// Look for any comments in db where post id matches
 	models.Comment.findAll({ where: { postId: postId } })
 		.then((result) => {
 			if (result.length > 0) {
+				// If result > 0 send response
 				res.status(200).json({
 					Comments: result,
 				});
 			} else {
+				// Send 404 error
 				res.status(404).json({
 					message: "No comments found!",
 				});
@@ -110,42 +122,47 @@ function showAll(req, res) {
 }
 /**
  *
- * @param {*} req
- * @param {*} res
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
  * @returns
  */
 function update(req, res) {
+	// id of post
 	const id = req.params.id;
+	// Updated comment object
 	const updatedComment = {
 		id: req.params.id,
 		content: req.body.content,
 		userId: req.userData.userId,
 	};
-	//
+	// Current user
 	const userId = req.userData.userId;
-
+	// Comment content validation
 	const schema = {
-		content: { type: "string", optional: false, max: "500" },
+		content: { type: "string", optional: false },
 	};
-
+	// new Validator
 	const v = new Validator();
+	// Validation updated comment against schema
 	const validationResponse = v.validate(updatedComment, schema);
-
+	// If validation not equal to true send Validation failed
 	if (validationResponse !== true) {
 		return res.status(400).json({
 			message: "Validation failed",
 			errors: validationResponse,
 		});
 	}
-
+	// If equal to true update comment where the id and userId match in db
 	models.Comment.update(updatedComment, { where: { id: id, userId: userId } })
 		.then((result) => {
 			if (result > 0) {
+				// Send updated comment
 				res.status(200).json({
 					message: "Comment updated successfully",
 					comment: updatedComment,
 				});
 			} else {
+				// Send 404 comment not found
 				res.status(404).json({
 					message: "Comment not found!",
 				});
@@ -159,14 +176,16 @@ function update(req, res) {
 		});
 }
 /**
- *
- * @param {*} req
- * @param {*} res
+ * Deletes one comment
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
  */
 function destroy(req, res) {
+	// Comment id
 	const id = req.params.id;
+	// Current user
 	const userId = req.userData.userId;
-
+	// Destroy comment where id and current user match
 	models.Comment.destroy({ where: { id: id, userId: userId } })
 		.then((result) => {
 			if (result > 0) {
@@ -186,7 +205,7 @@ function destroy(req, res) {
 			});
 		});
 }
-
+// Exports all functions
 module.exports = {
 	save: save,
 	showAll: showAll,
--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -25,13 +25,18 @@ import {
 	IconButton,
 	InputLabel,
 	Select,
+	Stack,
+	Switch,
 	TextareaAutosize,
+	Typography,
 } from "@mui/material";
 import axios from "axios";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { useRef } from "react";
+import { styled } from "@mui/material/styles";
+
 const Post = ({ post }) => {
 	// Get current user under the Alias currentUser
 	const { user: currentUser } = useContext(AuthContext);
@@ -288,6 +293,50 @@ const Post = ({ post }) => {
 		};
 		fetchUser();
 	}, [post.userId]);
+	const AntSwitch = styled(Switch)(({ theme }) => ({
+		width: 28,
+		height: 16,
+		padding: 0,
+		display: "flex",
+		"&:active": {
+			"& .MuiSwitch-thumb": {
+				width: 15,
+			},
+			"& .MuiSwitch-switchBase.Mui-checked": {
+				transform: "translateX(9px)",
+			},
+		},
+		"& .MuiSwitch-switchBase": {
+			padding: 2,
+			"&.Mui-checked": {
+				transform: "translateX(12px)",
+				color: "#fff",
+				"& + .MuiSwitch-track": {
+					opacity: 1,
+					backgroundColor:
+						theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+				},
+			},
+		},
+		"& .MuiSwitch-thumb": {
+			boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+			width: 12,
+			height: 12,
+			borderRadius: 6,
+			transition: theme.transitions.create(["width"], {
+				duration: 200,
+			}),
+		},
+		"& .MuiSwitch-track": {
+			borderRadius: 16 / 2,
+			opacity: 1,
+			backgroundColor:
+				theme.palette.mode === "dark"
+					? "rgba(255,255,255,.35)"
+					: "rgba(0,0,0,.25)",
+			boxSizing: "border-box",
+		},
+	}));
 
 	return (
 		<>
@@ -317,6 +366,13 @@ const Post = ({ post }) => {
 								<b>{post.moodId === "--" ? (post.moodId = "") : post.moodId}</b>
 							</span>
 						</div>
+
+						<Stack direction="row" spacing={1} alignItems="center">
+							<Typography>Unread</Typography>
+							<AntSwitch inputProps={{ "aria-label": "ant design" }} />
+							<Typography>Read</Typography>
+						</Stack>
+
 						{post.userId === currentUser.user.id && (
 							<div className="postTopRight">
 								<div className="postMenuWrapper">
