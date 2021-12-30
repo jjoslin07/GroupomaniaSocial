6dc952d1951e3d41b6559022ab09ee9968fc301b
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2021-12-29 19:59:01 -0800

    :unread badge

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import "./post.css";
+
 import {
 	AddAPhoto,
 	CancelPresentationOutlined,
@@ -15,6 +16,7 @@ import Comments from "../comments/Comments";
 
 import {
 	Avatar,
+	Badge,
 	Box,
 	Dialog,
 	DialogActions,
@@ -25,17 +27,13 @@ import {
 	IconButton,
 	InputLabel,
 	Select,
-	Stack,
-	Switch,
 	TextareaAutosize,
-	Typography,
 } from "@mui/material";
 import axios from "axios";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { useRef } from "react";
-import { styled } from "@mui/material/styles";
 
 const Post = ({ post }) => {
 	// Get current user under the Alias currentUser
@@ -241,50 +239,6 @@ const Post = ({ post }) => {
 		};
 		fetchUser();
 	}, [post.userId]);
-	const AntSwitch = styled(Switch)(({ theme }) => ({
-		width: 28,
-		height: 16,
-		padding: 0,
-		display: "flex",
-		"&:active": {
-			"& .MuiSwitch-thumb": {
-				width: 15,
-			},
-			"& .MuiSwitch-switchBase.Mui-checked": {
-				transform: "translateX(9px)",
-			},
-		},
-		"& .MuiSwitch-switchBase": {
-			padding: 2,
-			"&.Mui-checked": {
-				transform: "translateX(12px)",
-				color: "#fff",
-				"& + .MuiSwitch-track": {
-					opacity: 1,
-					backgroundColor:
-						theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
-				},
-			},
-		},
-		"& .MuiSwitch-thumb": {
-			boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
-			width: 12,
-			height: 12,
-			borderRadius: 6,
-			transition: theme.transitions.create(["width"], {
-				duration: 200,
-			}),
-		},
-		"& .MuiSwitch-track": {
-			borderRadius: 16 / 2,
-			opacity: 1,
-			backgroundColor:
-				theme.palette.mode === "dark"
-					? "rgba(255,255,255,.35)"
-					: "rgba(0,0,0,.25)",
-			boxSizing: "border-box",
-		},
-	}));
 
 	return (
 		<>
@@ -313,13 +267,15 @@ const Post = ({ post }) => {
 								{post.moodId ? post.moodId !== "--" && " feeling " : " "}
 								<b>{post.moodId === "--" ? (post.moodId = "") : post.moodId}</b>
 							</span>
+							<span>
+								<Badge
+									color="primary"
+									overlap="circular"
+									badgeContent=" unread"
+									sx={{ marginLeft: 5 }}
+								></Badge>
+							</span>
 						</div>
-
-						<Stack direction="row" spacing={1} alignItems="center">
-							<Typography>Unread</Typography>
-							<AntSwitch inputProps={{ "aria-label": "ant design" }} />
-							<Typography>Read</Typography>
-						</Stack>
 
 						{post.userId === currentUser.user.id && (
 							<div className="postTopRight">
